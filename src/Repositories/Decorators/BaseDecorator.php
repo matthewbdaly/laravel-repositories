5bf8b06a0954c7f3eb7f82b53d09c8187335bcdd
Matthew Daly <matthewbdaly@gmail.com>
2017-11-14 17:44:21 +0000

    Some progress fixing style violations

--- src/Repositories/Decorators/BaseDecorator.php
+++ src/Repositories/Decorators/BaseDecorator.php
@@ -4,14 +4,37 @@ namespace Matthewbdaly\LaravelRepositories\Repositories\Decorators;
 
 use Matthewbdaly\LaravelRepositories\Repositories\Interfaces\AbstractRepositoryInterface;
 
+/**
+ * Base decorator class
+ */
 abstract class BaseDecorator implements AbstractRepositoryInterface
 {
+    /**
+     * The repository to be used by the decorator
+     *
+     * @var object
+     */
     protected $repository;
 
+    /**
+     * The cache implementation
+     *
+     * @var object
+     */
     protected $cache;
 
+    /**
+     * The model to be used by the repository
+     *
+     * @var object
+     */
     protected $model;
 
+    /**
+     * Get all rows
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function all()
     {
         return $this->cache->tags($this->getModel())->remember('all', 60, function () {
@@ -19,38 +42,77 @@ abstract class BaseDecorator implements AbstractRepositoryInterface
         });
     }
 
-    public function find($id)
+    /**
+     * Get single row
+     *
+     * @param integer $id The object ID.
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function find(int $id)
     {
         return $this->cache->tags($this->getModel())->remember($id, 60, function () use ($id) {
             return $this->repository->find($id);
         });
     }
 
-    public function findOrFail($id)
+    /**
+     * Get single row, or throw a 404
+     *
+     * @param integer $id The object ID.
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function findOrFail(int $id)
     {
         return $this->cache->tags($this->getModel())->remember($id, 60, function () use ($id) {
             return $this->repository->findOrFail($id);
         });
     }
 
+    /**
+     * Create row
+     *
+     * @param array $input The input data.
+     * @return \Illuminate\Database\Eloquent\Model
+     */
     public function create(array $input)
     {
         $this->cache->tags($this->getModel())->flush();
         return $this->repository->create($input);
     }
 
-    public function update($id, array $input)
+    /**
+     * Update row
+     *
+     * @param integer $id    The object ID.
+     * @param array   $input The input data.
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function update(int $id, array $input)
     {
         $this->cache->tags($this->getModel())->flush();
         return $this->repository->update($id, $input);
     }
 
-    public function delete($id)
+    /**
+     * Delete row
+     *
+     * @param integer $id The object ID.
+     * @return null
+     */
+    public function delete(int $id)
     {
         $this->cache->tags($this->getModel())->flush();
         return $this->repository->delete($id);
     }
 
+    /**
+     * Get rows matching parameters
+     *
+     * @param array  $parameters
+     * @param string $field
+     * @param string $order
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function where(array $parameters, $field = null, $order = null)
     {
         $key = "";
@@ -68,6 +130,13 @@ abstract class BaseDecorator implements AbstractRepositoryInterface
         });
     }
 
+    /**
+     * Get a single row matching parameters
+     *
+     * @param integer $id
+     * @param array   $parameters
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function findWhere($id, array $parameters)
     {
         $key = $id . "_";
@@ -79,6 +148,13 @@ abstract class BaseDecorator implements AbstractRepositoryInterface
         });
     }
 
+    /**
+     * Get a single row matching parameters, or throw a 404
+     *
+     * @param integer $id
+     * @param array   $parameters
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function findWhereOrFail($id, array $parameters)
     {
         $key = $id . "_";
@@ -90,11 +166,22 @@ abstract class BaseDecorator implements AbstractRepositoryInterface
         });
     }
 
+    /**
+     * Set includes
+     *
+     * @param array $input
+     * @return \Matthewbdaly\LaravelRepositories\Repositories\Base
+     */
     public function with(array $tables)
     {
         return $this->repository->with($tables);
     }
 
+    /**
+     * Get model name
+     *
+     * @return string
+     */
     protected function getModel()
     {
         return $this->model;
